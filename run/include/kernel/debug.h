c0390a326de5bb3198daa8471b31bf9dcc2e4f32
Lizord <mt.demakov@gmail.com>
2024-07-20 21:01:16 +0300

    rm run

--- run/include/kernel/debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef __K_DEBUG_H
-#define __K_DEBUG_H 1
-
-#include <dev/serial.h>
-
-#define DEBUG_PUTCHAR(c) k_dev_serial_putchar(COM1, c)
-#define DEBUG_PUTSTR(str) k_dev_serial_putstr(COM1, str)
-
-#endif
--- run/include/stdio.h
+++
@@ -1,4 +0,0 @@
-#ifndef __LIBC_STDIO_H
-#define __LIBC_STDIO_H 1
-
-#endif
